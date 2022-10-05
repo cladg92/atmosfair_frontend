f569427de816d25fdcac724ead9cbb65993852cf
cladg92 <cladg92@gmail.com>
2022-10-05 09:44:16 +0200

    change var to let

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,7 @@
 // Extract list of airports names from data
 export const extractAirports = (list) => {
-  var extractAirports = list.map((item) => item.name);
+  let extractAirports = list.map((item) => item.name);
   // remove duplicates
-  var airports = [...new Set(extractAirports)];
+  let airports = [...new Set(extractAirports)];
   return airports;
 };
